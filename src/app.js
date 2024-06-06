0523fb3e47eb75d4e1e1ae40b969d151c1b9758e
vitornoms <vitornoms@gmail.com>
2024-06-06 09:05:35 -0300

    api pronta

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ const express = require('express');
 // Importar pacote dotenv, gerenciador de variáveis de ambiente
 const dotenv = require('dotenv').config();
 
+const taskRouter = require('./routes/taskRouter');
+const cors = require('cors');
+
 // Instanciar o express na variável app
 const app = express();
 
 // Setar a porta do servidor, a partir do arquivo .env ou assumir 3005
 app.set('port', process.env.PORT || 3005)
+
+app.use(express.json());
+app.use(cors());
+app.use('/api', taskRouter);
 
 module.exports = app;
---
+++ src/controller/taskController.js
@@ -0,0 +1,36 @@
+const connection = require('../config/db');
+const dotenv = require('dotenv').config();
+
+async function storeTask(request, response){
+    
+    const params = Array(
+        request.body.title,
+        request.body.description
+    );
+
+    const query = "INSERT INTO tasks(title,description) VALUES(?,?)";
+
+    connection.query(query, params, (err, results) => {
+        if(results) {
+            response
+            .status(201)
+            .json({
+                success: true,
+                message: "Sucesso!",
+                data: results
+            })
+        } else {
+            response
+            .status(400)
+            .json({
+                success: false,
+                message: "Ops, deu problema!",
+                sql: err
+            })
+        }
+    })
+}
+
+module.exports = {
+    storeTask
+}
