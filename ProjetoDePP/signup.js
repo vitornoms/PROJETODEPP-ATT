24fbbe3d7c48bdbc39152bfd21c2fc7376a88554
vitornoms <vitornoms@gmail.com>
2024-07-08 08:22:27 -0300

    continuar registro

--- ProjetoDePP/signup.js
+++ ProjetoDePP/signup.js
@@ -129,3 +129,27 @@ btnConfirm.addEventListener('click', ()=>{
     inputConfirmSenha.setAttribute('type', 'password')
   }
 })
+
+let button = document.getElementById("handleSubmit");
+
+button.onclick = async function() {
+    let nome = document.getElementById("nome").value;
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+    let confirmSenha = document.getElementById("confirmSenha").value;
+    let data = {title, usuario, senha, confirmSenha}
+
+    const response = await fetch('http://localhost:3000/api/store/task', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(data)
+    });
+
+    let content = await response.json();
+
+    if(content.success) {
+        alert("Sucesso")
+    } else{
+        alert('Não');
+    }
+}
