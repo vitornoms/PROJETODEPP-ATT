9855771a9b5a358d4a979de5132dce844b7d1129
vitornoms <vitornoms@gmail.com>
2024-08-12 08:45:58 -0300

    n funfa +

--- bd.sql
+++ bd.sql
@@ -14,8 +14,11 @@ create table users(
 	id int not null auto_increment primary key,
     name varchar(255) not null,
     password varchar(255) not null,
+    email varchar(255) not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
 select*from users;
+
+DROP DATABASE db_tasks;
